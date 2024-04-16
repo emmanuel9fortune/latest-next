c4a6a0371c60698dff501aaa3ccf51065bd4fb5a
emmanuel fortune <108401233+emmanuel9fortune@users.noreply.github.com>
2024-04-16 04:44:05 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -16,6 +16,7 @@ function MyApp({ Component, pageProps }) {
         <>
             <Head>
                 <link rel="shortcut icon" href="/log.jpg" type="image/x-icon" />
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
                 <title>Cat Ai</title>
             </Head>
                     <WalletConnectionProvider>
