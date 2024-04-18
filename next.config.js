a865f5362ea920b73740c69903f7e58fdad525a1
emmanuel fortune <108401233+emmanuel9fortune@users.noreply.github.com>
2024-04-18 20:55:56 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -5,7 +5,6 @@ module.exports = {
   swcMinify: true,
   images: {
     domains: ['gravatar.com'], // Add 'gravatar.com' to the list of allowed domains
-    formats: ["image/jpg"],
     unoptimized: true,
     remotePatterns:[
       {
